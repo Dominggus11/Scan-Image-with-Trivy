ac1870c4cbf01051d1dc83a7a37adf84ff337e3d
Dominggus11 <roymalau12304@gmail.com>
2022-09-05 11:12:06 +0700

    After Meet With Sifu

--- trivy/cmd.go
+++ trivy/cmd.go
@@ -7,15 +7,16 @@ import (
 
 func GoTrivy() {
 	// to know where we now
-	cmdPwd := exec.Command("cd")
-	cmdPwd.Stdout = os.Stdout
-	cmdPwd.Run()
+	// cmdPwd := exec.Command("cd")
+	// cmdPwd.Stdout = os.Stdout
+	// cmdPwd.Run()
 
 	cmd := exec.Command("pwd")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 
-	cmdTrivy := exec.Command("trivy", "image", "-f", "json", "-o", "resultsImage.json", "golang:1.12-alpine")
+	cmdTrivy := exec.Command("trivy", "config", "-f", "json", "-o", "/home/rdam/Project_Trivy/DockerFile/resultsImage.json", "Dockerfile")
+	cmdTrivy.Dir = "/home/rdam/build/"
 	cmdTrivy.Stdout = os.Stdout
 	cmdTrivy.Run()
 }
